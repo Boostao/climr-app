36d73376ec092affb31518eab68d8c097f276c6b
Bruno Tremblay <bruno@boostao.ca>
2025-03-25 12:20:34 -0400

    missed one

--- scripts/geometry.R
+++ scripts/geometry.R
@@ -327,7 +327,7 @@ session_geometry <- function() {
               xyz = xyz,
               which_refmap = vstore[["downscale_which_refmap"]],
               obs_periods = vstore[["downscale_obs_periods"]] |> n(),
-              obs_years  = vstore[["downscale_obs_years "]] |> n(),
+              obs_years  = vstore[["downscale_obs_years"]] |> n(),
               obs_ts_dataset = vstore[["downscale_obs_ts_dataset"]] |> n(),
               gcms = vstore[["downscale_gcms"]] |> n(),
               ssps = vstore[["downscale_ssps"]] |> n(),
